75cfbaeb4d7f0879ae1468941a7fc2716ff53775
Scott Conway <scoot1243@gmail.com>
2017-03-07 13:03:13 -0500

    removing config file from gitignore so heroku can read it

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  twitter: {
+    consumer_key: 'pjnT8gG8S0HOHi4DoFMnX7FYI',
+	consumer_secret: 'wT7I50Rd3MFeqbcf2nV085CioF25TqcEGicVP3b07dgB9xmR0K',
+	access_token_key: '2978700190-W1fi6trBPC1907noML4xD6111EqdvrEj1ORiiMm',
+	access_token_secret: 'UuskPa1SLWaKE2rkbN0y9w6QQCmen8EYu9Kt02tZ5MER0'
+  }
+}
